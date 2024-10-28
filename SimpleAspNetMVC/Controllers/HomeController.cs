44232ef9ef7478887e08afbc5f697e12ef980957
Simon Herkert <simon.herkert@outlook.com>
2024-10-28 09:48:46 +0200

    Css-styling, Layout

--- SimpleAspNetMVC/Controllers/HomeController.cs
+++ SimpleAspNetMVC/Controllers/HomeController.cs
@@ -12,14 +12,14 @@ namespace SimpleAspNetMVC.Controllers
         {
             return View();
         }
-
+        [AllowAnonymous]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
-
+        [Authorize]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
